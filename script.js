113dbad98a25b55e0bc336be8d7694aa74c52d75
tijsv <tijs.verellen@hotmail.com>
2018-05-31 16:17:17 +0200

    RSS test2

--- script.js
+++ script.js
@@ -121,18 +121,55 @@ function getLiveStreams() {
   });
 }
 
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
 function getRSSFeed() {
 
-  $.ajax({
-    type: 'GET',
-    url: 'https://www.vrt.be/vrtnieuws/nl.rss.articles.xml',
-    headers: {
-      'Access-Control-Allow-Origin': true
-    },
-    success: function(RSSFeed) {
-      console.log(RSSFeed);
-    }
-  });
+  var url = "https://www.vrt.be/vrtnieuws/nl.rss.articles.xml";
+
+  var xhr = createCORSRequest('GET', url);
+  if (!xhr) {
+    throw new Error('CORS not supported');
+  } else {
+    console.log(xhr);
+  }
+
+  xhr.withCredentials = true;
+
+  xhr.send();
+
+  xhr.onload = function() {
+   var responseText = xhr.responseText;
+   console.log(responseText);
+   // process the response.
+  };
+
+  xhr.onerror = function() {
+    console.log('There was an error!');
+  };
+
 
 }
 
